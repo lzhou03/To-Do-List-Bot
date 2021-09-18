5019f382a8aa94b338bf52a2cae33f803910bce7
YesIStoleTheCookies <55215582+YesIStoleTheCookies@users.noreply.github.com>
2021-09-18 10:32:52 -0400

    added npm mongoose

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-require("dotenv").config();
+//require("dotenv").config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const mongoose = require('mongoose');
 
 client.once('ready', () => {
 	console.log('Ready!');
