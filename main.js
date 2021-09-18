622c0c64da07138fb7ef5bbe6668a3c4490055fb
YesIStoleTheCookies <55215582+YesIStoleTheCookies@users.noreply.github.com>
2021-09-18 10:54:55 -0400

    added commands folder

--- main.js
+++ main.js
@@ -5,9 +5,35 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const { prefix } = require('./config.json');
 
+
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+
+
 client.once('ready', () => {
 	console.log('Ready!');
   client.channels.cache.get("888790836094066732").send("ready");
 });
 
+
+
+
+
+
+mongoose.connect(process.env.MONGODB_SRV, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //userFindAndModify: false,
+}).then(()=>{
+  console.log("Connected to DB");
+}).catch((err) => {
+  console.log(err);
+});
 client.login(process.env.BOT_TOKEN);
