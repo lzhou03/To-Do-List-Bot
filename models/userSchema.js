69c3290372ac5e13262bd5d7ffff081f7c7b8fb5
YesIStoleTheCookies <55215582+YesIStoleTheCookies@users.noreply.github.com>
2021-09-19 21:05:47 -0400

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   optIn: Boolean,
   lastDate: Date,
   lastList: [String],
-  lastListDate: {type: Date, default: '9/28/2003'},
+  lastListDate: {type: Date, default: "09/28/2003"},
   lastListAll:[String]
 });
 const user = mongoose.model('User', userSchema);
