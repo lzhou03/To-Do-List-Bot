2845fa9c26913ba157425dee60d4e566f19448f2
Laura Zhou <55215582+YesIStoleTheCookies@users.noreply.github.com>
2021-11-22 21:39:23 +0000

    e

--- main.js
+++ main.js
@@ -10,80 +10,80 @@ const { MessageEmbed } = require('discord.js');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles){
+for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
   //client.channels.cache.get("888802387983212554").send("ready");
-	client.user.setActivity(`over u`, {type: 'WATCHING'});
+  client.user.setActivity(`over u`, { type: 'WATCHING' });
 });
 
 //what the bot does whenever a message is sent
 client.on('message', message => {
-	//if the author is the bot, ignore the message
-	if(message.author.id === client.user.id) return;
+  //if the author is the bot, ignore the message
+  if (message.author.id === client.user.id) return;
 
-	//if the message starts with the prefix, execute this.
-	if(message.content.startsWith(`${prefix}`)){
-		//separate commands and arguments
-		const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
+  //if the message starts with the prefix, execute this.
+  if (message.content.toLowerCase()startsWith(`${prefix}`)){
+  //separate commands and arguments
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
 
-		if(command === 'hello' || command === 'Hello'){
-			message.channel.send('Hi there!');
-		}
-		else if(command === 'add'){
-			//message.channel.send("abcd");
-			client.commands.get("add").execute(message, args, command, client, Discord, db)
+  if (command === 'hello' || command === 'Hello') {
+    message.channel.send('Hi there!');
+  }
+  else if (command === 'add') {
+    //message.channel.send("abcd");
+    client.commands.get("add").execute(message, args, command, client, Discord, db)
 
-		}
-		else if(command === 'rm' || command == 'remove'){
-			//message.channel.send("abcd");
-			client.commands.get("rm").execute(message, args, command, client, Discord, db);
-		}
-		else if(command === 'list'){
-			//message.channel.send("abcd");
-			client.commands.get("list").execute(message, args, command, client, Discord, db);
-		}
-		else if(command === 'done'){
-			//message.channel.send("abcd");
-			client.commands.get("done").execute(message, args, command, client, Discord, db);
+  }
+  else if (command === 'rm' || command == 'remove') {
+    //message.channel.send("abcd");
+    client.commands.get("rm").execute(message, args, command, client, Discord, db);
+  }
+  else if (command === 'list') {
+    //message.channel.send("abcd");
+    client.commands.get("list").execute(message, args, command, client, Discord, db);
+  }
+  else if (command === 'done') {
+    //message.channel.send("abcd");
+    client.commands.get("done").execute(message, args, command, client, Discord, db);
 
-		}
-		else if(command === 'edit'){
-			//message.channel.send("abcd");
-			client.commands.get("edit").execute(message, args, command, client, Discord, db);
+  }
+  else if (command === 'edit') {
+    //message.channel.send("abcd");
+    client.commands.get("edit").execute(message, args, command, client, Discord, db);
 
-		}
-		else if(command === 'clear'){
-			//message.channel.send("abcd");
-			client.commands.get("clear").execute(message, args, command, client, Discord, db);
+  }
+  else if (command === 'clear') {
+    //message.channel.send("abcd");
+    client.commands.get("clear").execute(message, args, command, client, Discord, db);
 
-		}
-		else {
-			const embed = new MessageEmbed()
-			.setColor("RED")
-			.setTitle("Invalid Command!")
-			.setDescription(command);
+  }
+  else {
+    const embed = new MessageEmbed()
+      .setColor("RED")
+      .setTitle("Invalid Command!")
+      .setDescription(command);
 
-			message.channel.send(embed);
-		}
-	}
+    message.channel.send(embed);
+  }
+}
 	else if (message.content.startsWith("tittie!")) {
-		message.reply("Disgusting.");
-		message.react('🤮');
+  message.reply("Disgusting.");
+  message.react('🤮');
 
-	}
+}
 })
 
 mongoose.connect(process.env.MONGODB_SRV, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   //userFindAndModify: false,
-}).then(()=>{
+}).then(() => {
   console.log("Connected to DB");
 }).catch((err) => {
   console.log(err);
