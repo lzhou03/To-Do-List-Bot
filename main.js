156659b8d6ba5477a3359f14e7bd63e119900387
YesIStoleTheCookies <55215582+YesIStoleTheCookies@users.noreply.github.com>
2022-01-16 22:58:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ client.on('message', message => {
   if (message.content.toLowerCase().startsWith(`${prefix}`)){
   //separate commands and arguments
   const args = message.content.slice(prefix.length).trim().split(' ');
-  const command = args[0].toLowerCase();
+  const command = args.shift().toLowerCase();
 
   if (command === 'hello' || command === 'Hello') {
     message.channel.send('Hi there!');
